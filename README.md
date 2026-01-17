9c28da671c9f19cc01dea9e11455d367b019e6fb
Wilhelmina <bzzz@hommel.lt>
2026-01-17 13:05:27 +0000

    version 0.4.0 - support for OUY and removed summer tentamenweek

--- README.md
+++ README.md
@@ -2,16 +2,17 @@
 This plugin started out as a fork of https://github.com/IMB11/ObsidianLUWeeks. It has digressed enough to make it a standalone project, but not without proper thanks to IMB11 for the idea and a basis to work off!
 
 # Open Universiteit Period Plugin
+This plugin extends Obsidian’s built-in Moment.js features to provide the custom date tokens **OUW**, **OUQ**, and **OUY**. When you set your Daily Note (or other Obsidian date formatting options) to include `OUW`, `OUQ`, or `OUY` it will be replaced with either:
 
-This plugin extends Obsidian’s built-in Moment.js features to provide custom date tokens called **OUW** and **OUQ**. When you set your Daily Note (or other Obsidian date formatting options) to include "OUW" or "OUQ" it will be replaced with either:
+| Token | Replacement  | Explanation                                                      |
+|-------|--------------|------------------------------------------------------------------|
+| OUW   | "#"          | **Weeks 1–10** of the quarter (replace # with week number)       |
+| OUW   | "T"          | **Final week** of the quarter ("T" = tentamenweek)               |
+| OUW   | "Kerstreces" | **Christmas break**                                              |
+| OUQ   | "#"          | **Quarter number** (replace # with quarter)                      |
+| OUQ   | "Z"          | **Between Q4 and next academic year** ("Z" = zomer)              |
+| OUY   | "##-##"      | **Academic year** (replace ## with YY for start and end)         |
 
-| Token | Replacement    | Explanation                                                 |
-|-------|----------------|-------------------------------------------------------------|
-| OUW   | "Week #"       | For weeks 1 - 10 of the quarter, where # is the week number |
-| OUW   | "Tentamenweek" | For the last week of the quarter or the last week of summer |
-| OUW   | "Kerstreces"   | For the Christmas break                                     |
-| OUQ   | "Q#"           | Where # is the quarter number                               |
-| OUQ   | "Zomer"        | Between Q4 and the next academic year                       | 
 
 ## OU Academic Year
 The week numbers in below table are ISO8601 weeknumbers. 
@@ -24,24 +25,25 @@ The week numbers in below table are ISO8601 weeknumbers.
 | 4       | 18         | 28        |
 | Summer  | 29         | 35        |
 
-> The OU academic year has followed above pattern for several years now, but this is not an official schedule. After the study guides for the next academic year are released, I will ensure they continue to do so, or update the plugin accordingly.
+> The OU academic year has followed above pattern for several years now, but this is not an official schedule. After the study guides for each next academic year are released I will ensure they continue to do so, or update the plugin accordingly.
 ## Usage
+### Installation
+1. There are several ways to install the plugin, pick your favorite: 
+   - Copy or clone the plugin files into your Obsidian vault’s plugins folder.
+   - ~~Alternatively you can download it from the Community Plugin Browser.~~ -> not at the moment, hopefully soon.
+2. Ensure that you have enabled it in Obsidian’s "Community Plugins" section.
 
-1. **Install the Plugin**  
-   - Copy or clone the plugin files into your Obsidian vault’s plugins folder.  
-   - ~~Alternatively you can download it from the Community Plugin Browser.~~ -> not at the moment, hopefully later.
-   - Ensure that you have enabled it in Obsidian’s "Community Plugins" section.
-
-2. **Set Your Daily Note Date Format**  
+### Use with Daily Notes/Periodic Notes/Templater and others
+You can now use the tokens in any place where you can use moment.js formatting. For example to use in your Daily Notes:
    - Go to Obsidian’s "Settings" → "Core Plugins" → "Daily Notes."  
-   - In the **Date Format** field, include `OUW` or `OUQ`.  
-   - Example: `OUQ - OUW`
-
-3. **Verify the Output**  
-   - Create a new daily note or check your daily note for a date within the defined ranges.  
-   - Confirm that "OUW" is replaced with the correct "Week X".
+   - In the **Date Format** field, include `OUW`, `OUQ`, `OUY`.  
+   - Example: `OUY [Q]OUQ - [week] OUW` returns something like "2525 Q3 - week 7"
 
 ## Releases
+### 0.4.0
+- Remove summer "tentamenweek" as they won't take place anymore starting this academic year
+- Added token `OUY` for the academic year
+
 ### 0.3.0
 - Plugin cleans up after itself on unload
 
--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-// An Obsidian plugin that registers custom "OUW" and OUQ format token in moment.js.
-// You can then use "OUW" or "OUQ" in your Daily Note plugin's date format, for instance: 
-//     [2425] OUQ OUW
-// This will display "2425 Q4 Week 1" or "2425 Q3 Tentamenweek" in the 11th week of the quarter.
+// An Obsidian plugin that registers custom "OUW", "OUQ", and "OUY" format token in moment.js.
+// You can then use "OUW", "OUQ", or "OUY" in your Daily Note plugin's date format, for instance: 
+//     OUY [ Q]OUQ [ Week ]OUW
+// This will display "2425 Q4 Week 1" or "2425 Q3 Week T" (=tentamenweek) in the 11th week of the quarter.
 
 const { Plugin } = require("obsidian");
 // save this puppy for unloading
-const originalFormat =  window.moment.fn.format;
+const originalFormat = window.moment.fn.format;
 
-const ZOMER = "Zomer";
+const ZOMER = "Z";
 
 const ouPeriods = [
-  {q: "Q1", start: 36, end: 46},
-  {q: "Q2", start: 47, end: 6}, // weken 53 en 1 zijn kerstreces
-  {q: "Q3", start: 7, end: 17},
-  {q: "Q4", start: 18, end: 28},
-  {q: ZOMER, start: 29, end: 35}
+  { q: "1", start: 36, end: 46 },
+  { q: "2", start: 47, end: 6 }, // weken 53 en 1 zijn kerstreces
+  { q: "3", start: 7, end: 17 },
+  { q: "4", start: 18, end: 28 },
+  { q: ZOMER, start: 29, end: 35 }
 ]
 
 const enhanceMomentWithOUPeriods = () => {
@@ -22,30 +22,30 @@ const enhanceMomentWithOUPeriods = () => {
   const getOUPeriod = function (isoWeekNumber) {
 
     for (const period of ouPeriods) {
-      if (isoWeekNumber >= period.start && isoWeekNumber <=period.end) {
+      if (isoWeekNumber >= period.start && isoWeekNumber <= period.end) {
         return period;
       }
     }
-    // It must be q2, the weird one.
+    // It must be q2, the weird one with a higher number for start than for end
     return ouPeriods[1];
   }
 
   // If ouWeek() is not already defined, we add it
   if (!window.moment.prototype.ouWeek) {
     window.moment.prototype.ouWeek = function (date) {
-      
+
       const isoWeekNumber = date.isoWeek();
       const ouPeriod = getOUPeriod(isoWeekNumber);
-      
-        if ([53,1].includes(isoWeekNumber)) {
-            return "Kerstreces";
-        } else if (isoWeekNumber < ouPeriods[1].end) {
-            return `Week ${isoWeekNumber + 5}`;
-        } else if (isoWeekNumber == ouPeriod.end) {
-            return "Tentamenweek";
-        } else {
-            return `Week ${isoWeekNumber - ouPeriod.start + 1}`;
-        }
+
+      if ([53, 1].includes(isoWeekNumber)) {
+        return "Kerstreces";
+      } else if (isoWeekNumber < ouPeriods[1].end) {
+        return `${isoWeekNumber + 5}`;
+      } else if (isoWeekNumber == ouPeriod.end & !(isoWeekNumber == 35)) {
+        return "T";
+      } else {
+        return `${isoWeekNumber - ouPeriod.start + 1}`;
+      }
     }
   };
 
@@ -57,9 +57,23 @@ const enhanceMomentWithOUPeriods = () => {
       return ouPeriod.q;
     };
   }
-  
 
-  // Patch moment's format() to replace "OUW" and "OUQ" with the outputs of ouWeek() and ouQuarter() respectively
+  // If ouYear() is not already defined, we add it
+  if (!window.moment.prototype.ouYear) {
+    window.moment.prototype.ouYear = function (date) {
+      const calendarYear = date.format("YY");
+      const prevYear = date.subtract(1,"y").format("YY");
+      const nextYear = date.add(1, "y").format("YY");
+
+      if (date.isoWeek() >= 36) {
+        return `${calendarYear}-${nextYear}}`;
+      } else {
+        return `${prevYear}-${calendarYear}`;
+      }
+    };
+  }
+
+  // Patch moment's format() to replace "OUW", "OUQ", and "OUY" with the outputs of ouWeek(), ouQuarter(), and ouYear() respectively
   // We do this only once, indicated by a custom flag _oupInjected.
   if (!window.moment.prototype._oupInjected) {
 
@@ -68,12 +82,16 @@ const enhanceMomentWithOUPeriods = () => {
         return originalFormat.call(this, formatStr);
       }
       if (formatStr.includes("OUW")) {
-        const ouWeekValue = this.ouWeek(this); // "Week 3" or "Tentamenweek"
+        const ouWeekValue = this.ouWeek(this); // "3" or "T"
         formatStr = formatStr.replace(/OUW/g, "[" + ouWeekValue + "]");
       }
       if (formatStr.includes("OUQ")) {
-        const ouQuarterValue = this.ouQuarter(this); // "Q3" or "Zomer"
+        const ouQuarterValue = this.ouQuarter(this); // "2" or "Z"
         formatStr = formatStr.replace(/OUQ/g, "[" + ouQuarterValue + "]");
+      }
+      if (formatStr.includes("OUY")) {
+        const ouYearValue = this.ouYear(this); // "2425"
+        formatStr = formatStr.replace(/OUY/g, "[" + ouYearValue + "]");
       }
       return originalFormat.call(this, formatStr);
     };
@@ -91,10 +109,11 @@ module.exports = class OUPeriodsFormatTokenPlugin extends Plugin {
   onunload() {
     if (window.moment.prototype._oupInjected) {
       window.moment.prototype.format = originalFormat;
-      
+
       delete window.moment.prototype._oupInjected;
       delete window.moment.prototype.ouWeek;
       delete window.moment.prototype.ouQuarter;
+      delete window.moment.prototype.ouYear;
     }
     console.log("OUPeriodsFormatTokenPlugin unloaded");
   }
